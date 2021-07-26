75aaf006b0c5da9bf38f361ea2f9e055b5099762
Kartik Ohri <kartikohri13@gmail.com>
2021-07-27 00:11:12 +0530

    Fix typo

--- listenbrainz_spark/request_consumer/request_consumer.py
+++ listenbrainz_spark/request_consumer/request_consumer.py
@@ -138,7 +138,7 @@ class RequestConsumer:
             try:
                 self.connect_to_rabbitmq()
                 self.init_request_channel()
-                self.init_request_channel()
+                self.init_result_channel()
                 logger.info('Request consumer started!')
 
                 try:
