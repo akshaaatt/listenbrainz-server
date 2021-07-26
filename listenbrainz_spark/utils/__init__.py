24dad883ec1170736fc12b86a6420a9b3cc0a979
Kartik Ohri <kartikohri13@gmail.com>
2021-07-26 14:56:44 +0530

    Remove custom rabbitmq heartbeat
    Currently, the heartbeat is not allowed to be more than 60s so there
    is no point in trying to set a heartbeat of 2 hrs. The behaviour of
    heartbeats has changed over rabbitmq versions we had probably added
    this when it was allowed to set such high heartbeats.

--- listenbrainz_spark/utils/__init__.py
+++ listenbrainz_spark/utils/__init__.py
@@ -55,7 +55,7 @@ def append(df, dest_path):
 
 
 def init_rabbitmq(username, password, host, port, vhost, log=logger.error,
-                  heartbeat=None, connection_name="listenbrainz-spark-request-consumer"):
+                  connection_name="listenbrainz-spark-request-consumer"):
     while True:
         try:
             credentials = pika.PlainCredentials(username, password)
@@ -64,7 +64,6 @@ def init_rabbitmq(username, password, host, port, vhost, log=logger.error,
                 port=port,
                 virtual_host=vhost,
                 credentials=credentials,
-                heartbeat=heartbeat,
                 client_properties={"connection_name": connection_name}
             )
             return pika.BlockingConnection(connection_parameters)
